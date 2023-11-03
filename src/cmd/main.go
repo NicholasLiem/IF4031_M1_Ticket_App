347de80fecaf28426464308fc0a116b0d830d707
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-11-03 13:43:45 +0700

    feat: delete booking entity

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -54,7 +54,7 @@ func main() {
 	/**
 	Run DB Migration
 	*/
-	datastruct.Migrate(db, &datastruct.Event{}, &datastruct.UserModel{}, &datastruct.Payment{}, &datastruct.Invoice{}, &datastruct.Booking{})
+	datastruct.Migrate(db, &datastruct.Event{}, &datastruct.UserModel{}, &datastruct.Payment{}, &datastruct.Invoice{})
 
 	/**
 	Setting up the router
--- src/internal/datastruct/invoice.go
+++ src/internal/datastruct/invoice.go
@@ -9,5 +9,4 @@ type Invoice struct {
 	TotalAmount   float64       `json:"total_amount,omitempty"`
 	PaymentStatus PaymentStatus `json:"payment_status,omitempty"`
 	DueDate       string        `json:"due_date,omitempty"`
-	Booking       Booking       `gorm:"foreignKey:BookingID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
